7fa40e4c9df41c00a3ef0abe9ce08c9af4dc36e8
Nicholas <nicholasbrown.us@gmail.com>
2023-02-13 15:23:15 -0500

    Lab 06 Initial Document Setup

---
+++ app.js
@@ -0,0 +1,31 @@
+`use strict`;
+
+
+
+// **** GLOBALS ****
+let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm', '7pm'];
+
+
+
+// **** HELPER FUNCTIONS/UTILITIES ****
+
+
+
+
+// **** OBJECT LITERAL ****
+
+let seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesBought: 6.3,
+  cookiesBought: [],
+  generateCookies: function(){
+    // Put calculation here
+  }
+};
+
+
+
+
+// **** DOM MANIPULATION ****
